e725d8a9ae0176b8ea08d19410cf949b82d7c87b
SSSuperNinja <117931557+SSSuperNinja@users.noreply.github.com>
2024-08-28 23:37:12 -0700

    Add files via upload

---
+++ SW P7.js
@@ -0,0 +1,6 @@
+const frutas = ["manzana", "banana", "pera"];
+const vegetales = ["zanahoria", "brócoli", "espinaca"];
+
+const alimentos = frutas.concat(vegetales);
+
+console.log("Arreglo concatenado:", alimentos);
